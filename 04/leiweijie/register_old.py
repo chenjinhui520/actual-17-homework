64e725d872b0bb9742f74419040605b7acafe483
woniuray <rayleiwj@qq.com>
2017-12-22 23:37:58 +0800

    update

--- 04/leiweijie/register_old.py
+++ 04/leiweijie/register_old.py
@@ -2,7 +2,16 @@
 # encoding: utf-8
 '''
 @author: leiweijie
-Create on2017年12月22日
+Create on2017年12月17日
+'''
+'''
+基于文件存储的交互式用户登录注册系统
+注册模块
+实现功能：
+    1.普通用户注册，并授权
+    2.超级管理员注册（admin），并授权
+使用方法：
+    1.运行reister.py
 '''
 def userIsNotIn(user_name,user_file='users.txt'):
     '''判断用户名是否存在'''
@@ -22,25 +31,22 @@ def fileWrite(user_name,user_passwd,user_file='users.txt'):
 def Register():
     '''用户注册'''
     user_name = raw_input('请输入用户名：').strip()
-    if not user_name:
-        print '用户名不能为空'
-        return
-    if userIsNotIn(user_name):
-        print '用户已存在'
-        return
-    print '根据提示设置密码'
-    user_passwd = raw_input('请输入密码：').strip()
-    if not user_passwd:
-        print '密码不能为空'
-        return
-    confirm_passwd = raw_input('请再次输入密码：').strip()
-    if user_passwd == confirm_passwd:
-        fileWrite(user_name,user_passwd)
-        print '注册成功'
+    if user_name:
+        if not userIsNotIn(user_name):
+            print '根据提示设置密码'
+            user_passwd = raw_input('请输入密码：').strip()
+            if user_passwd:
+                confirm_passwd = raw_input('请再次输入密码：').strip()
+                if user_passwd == confirm_passwd:
+                    fileWrite(user_name,user_passwd)
+                    print '注册成功'
+                else:
+                    print '密码不一致'
+            else:
+                print '密码不能为空'
+        else:
+            print '用户已存在'
     else:
-        print '密码不一致'
-
-
-
+        print '用户名不能为空'
 if __name__ == '__main__':
     Register()       
